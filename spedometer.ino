bf1dd3df90313025d8f0d02f26ac1bc951a840ae
MHafizudden <mhafizudden3@gmail.com>
2020-10-20 16:58:31 +0800

    test graphics

--- spedometer.ino
+++ spedometer.ino
@@ -4,11 +4,15 @@
 #include <SoftwareSerial.h>
 
 const uint8_t RXPin = 4, TXPin = 3;
+const uint8_t TFT_CS = 10, TFT_DC = 8, TFT_RST = 9; 
 const uint8_t GPSBaud = 9600;
 
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 TinyGPSPlus gps;
 SoftwareSerial ss(RXPin, TXPin);
+
+double lat = 3.12331211;
+double lng = 101.23441212;
 
 void setup() {
   // put your setup code here, to run once:
@@ -24,9 +28,26 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while(ss.available()){
+  while(ss.available() > 0){
     gps.encode(ss.read());
   }
+
+  //graphic test first!!
+  tft.setCursor(0, 0);
+  tft.println(F("Graphics test first!!!"));
+  tft.setTextSize(2);
+  tft.println(F("Hello world!!!"));
+  tft.setTextSize(1);
+  tft.print(F("Location is: "));
+  tft.print(lat, 6);
+  tft.print(", ");
+  tft.print(lng, 6);
+
+//  if(millis() > 5000 && gps.charsProcessed() < 10){
+//    tft.setCursor(0, 0);
+//    tft.fillScreen(ST77XX_BLACK);
+//    tft.print(F("Connection error. Check wiring"));
+//  }
   
   
 }
